48b7edb87bbdabd49483ef0acfe036b2642412fa
あまみや ゆうこ <1@1234.sh>
2015-08-10 20:07:10 +0800

    Update codecomb.droptxt.js

--- codecomb.droptxt.js
+++ codecomb.droptxt.js
@@ -1,27 +1,27 @@
-var lock = false;
 (function ($) {
-    $.fn.droptxt = function (url, field) {
+    $.fn.droptxt = function (url, field, target) {
         var selector = this.selector;
         this.attr('autocomplete', 'Off');
         var txt = this;
         this.on('keyup', function () {
-            if (lock) return;
-            lock = true;
             var args = {};
             args[field] = txt.val();
+            if (args[field] == null)
+                args[field] = "";
             $('.codecomb-droptxt-outer').remove();
             $.getJSON(url, args, function (data) {
                 var html = '<div class="codecomb-droptxt-outer" data-parent="' + selector + '">';
                 for (var i = 0; i < data.length; i++) {
-                    html += '<div class="codecomb-droptxt-item" onclick="$(\'' + selector + '\').val(\''+ data[i].Value +'\'); $(\'.codecomb-droptxt-outer\').remove()">' + data[i].Display + '</div>'
+                    html += '<div class="codecomb-droptxt-item" onclick="$(\'' + target + '\').val(\''+ data[i].Value +'\'); $(\''+ selector +'\').val(\'' + data[i].Display + '\'); $(\'.codecomb-droptxt-outer\').remove()">' + data[i].Display + '</div>'
                 }
                 html += '</div>';
                 var dom = $(html);
+                if ($(".codecomb-droptxt-outer").length > 0 && $(".codecomb-droptxt-outer").attr("data-parent") == selector)
+                    $(".codecomb-droptxt-outer[data-parent='" + selector + "']").remove();
                 $('body').prepend(dom);
                 dom.css('top', (txt.offset().top + txt.outerHeight()) + 'px');
                 dom.css('left', txt.offset().left);
                 dom.outerWidth(txt.outerWidth());
-                lock = false;
             });
         });
         $(document).on('click', function (e) {
